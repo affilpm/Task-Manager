30355db2a71d1e21d54340cc3e76cd8fea473c0e
affilpm <affilpm2004@gmail.com>
2025-05-12 23:13:10 +0530

    added imported Navigate from router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import VerifyOtp from './components/Registration/OtpVerification';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Register from './components/user/Registration/Register';
 import { LogIn } from 'lucide-react';
 import Login from './components/user/login/PasswordLogin';
